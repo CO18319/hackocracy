7e6f5bb7fdfcafbdd17aee967b5cebda1344340f
Deepinder Singh <deepindersingh653@gmail.com>
2020-11-01 23:24:48 +0530

    Update execute.js for redirection

--- execute.js
+++ execute.js
@@ -6,11 +6,14 @@ document.getElementById("execute").addEventListener("click", function() {
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("source_code").innerText = this.responseText;
             alert(this.responseText);
+            if(this.responseText == "Success")
+            {
+                setTimeout(window.location.replace("problems.php"), 10000);
+            }
         }
     };
-    xhttp.open("POST", "output.php?id=" + id + "&language=" + language + "&code=" + encodeURIComponent(code), true);
+    xhttp.open("POST", "program_files/output.php?id=" + id + "&language=" + language + "&code=" + encodeURIComponent(code), true);
     xhttp.send();   
   });
   
